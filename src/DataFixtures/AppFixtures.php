e3c8190e405a830ae250de4e33032b38c08fcfff
Strannik4884 <strannik4884@mail.ru>
2020-06-06 18:55:31 +0300

    Add login page and access control

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,38 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->passwordEncoder = $encoder;
+    }
+
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
+        // create user
+        $user = new User();
+        $user->setName('Тестов Тест Тестович');
+        $user->setEmail('test@test.ru');
+        $user->setPassword($this->passwordEncoder->encodePassword($user,'Test123!'));
+        $user->setRoles(array('ROLE_USER'));
+        // create admin
+        $admin = new User();
+        $admin->setName('Admin');
+        $admin->setEmail('admin@test.ru');
+        $admin->setPassword($this->passwordEncoder->encodePassword($admin, 'Odmin123!'));
+        $admin->setRoles(array('ROLE_ADMIN'));
+        // insert users to database
+        $manager->persist($user);
+        $manager->persist($admin);
+        // flush manager
         $manager->flush();
     }
 }
