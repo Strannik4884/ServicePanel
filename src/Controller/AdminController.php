8b6fa1084923aadecb73d1b72fb5f9a0fb72abbc
Strannik4884 <strannik4884@mail.ru>
2020-06-07 10:26:17 +0300

    Add rtf document upload form

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,18 +2,62 @@
 
 namespace App\Controller;
 
+use App\Entity\Document;
+use App\Form\DocumentType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class AdminController extends AbstractController
 {
     /**
      * @Route("/admin", name="admin")
+     * @param Request $request
+     * @param SluggerInterface $slugger
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
      */
-    public function index()
+    public function index(Request $request, SluggerInterface $slugger)
     {
-        return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
+        $document = new Document();
+        $form = $this->createForm(DocumentType::class, $document);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var UploadedFile $rtfFile */
+            $rtfFile = $form->get('src_rtf')->getData();
+
+            if ($rtfFile) {
+                $originalFilename = pathinfo($rtfFile->getClientOriginalName(), PATHINFO_FILENAME);
+                // this is needed to safely include the file name as part of the URL
+                $safeFilename = $slugger->slug($originalFilename);
+                /** @var TYPE_NAME $safeFilename */
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$rtfFile->guessExtension();
+
+                // Move the file to the directory where brochures are stored
+                try {
+                    $rtfFile->move(
+                        $this->getParameter('docs_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                }
+
+                // updates the 'brochureFilename' property to store the PDF file name
+                // instead of its contents
+                $document->setSrcRtf($newFilename);
+            }
+
+            // ... persist the $document variable or any other work
+
+            return $this->redirect($this->generateUrl('admin'));
+        }
+
+        return $this->render('document/new.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
---
+++ src/Form/DocumentType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Document;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
+
+class DocumentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            // ...
+            ->add('src_rtf', FileType::class, [
+                'label' => 'Document (RTF file)',
+                'mapped' => false,
+                'required' => false,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1024k',
+                        'mimeTypes' => [
+                            'application/rtf',
+                        ],
+                        'mimeTypesMessage' => 'Пожалуйста, загрузите валидный RTF документ',
+                    ])
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Document::class,
+        ]);
+    }
+}
